d2b93dc49762e41dd9ed487807e00cce12c113b2
KyaDLFiles <103515463+KyaDLFiles@users.noreply.github.com>
2022-04-17 15:18:28 +0200

    Add info about fight

---
+++ btn_placer.py
@@ -0,0 +1,44 @@
+"""
+To save time, I made this script that allows to do this in the HTML files:
+Write #@ follower by the button as it is called in /common/btn/, without the .png
+Eg: #@o for circle
+Then, run this script and supply it the html file path, and it will replace all of the patterns with the corresponding img element for that button
+Bc I'm lazy, this will not alter the original filename, so replace it with the new one
+"""
+
+from sys import argv
+
+if (len(argv) < 2):
+    fp = input("File path: ")
+else:
+    fp = argv[1]
+
+fin = open(fp, "r")
+fout = open(fp + "2.html", "w")
+btns = {
+    "st": "Start",
+    "se": "Select",
+    "dd": "D-pad down",
+    "dr": "D-pad right",
+    "du": "D-pad up",
+    "dl": "D-pad left",
+    "r2": "R2",
+    "l2": "L2",
+    "r1": "R1",
+    "l1": "L1",
+    "r3": "R3",
+    "l3": "L3",
+    "x": "X", 
+    "o": "Circle",
+    "t": "Triangle",
+    "s": "Square", #Square at the end bc of st and se
+}
+
+for line in fin:
+    if "#@" in line:
+        print(line)
+        for i in btns:
+            line = line.replace("#@" + i, f'<img src="/common/btn/{i}.png" alt="{btns[i]}">')
+    
+    fout.write(line)
+    
