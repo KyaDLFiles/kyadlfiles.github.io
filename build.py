b8eab908467e34bc0e493e4e3bb8ff9b03792faf
The Kya: Dark Lineage Files <103515463+KyaDLFiles@users.noreply.github.com>
2025-08-06 17:20:02 +0200

    Better comments [skip ci]

--- build.py
+++ build.py
@@ -29,46 +29,48 @@ markdowner = markdown.Markdown(
     )
 )
 
-def dit_dict(name: str, dir: str) -> dict:
+def dir_dict(name: str, dir: str) -> dict:
     return {"name": name, "dir": dir, "children": []}
 
 def md_context(template):
     global dirs
+    # Read and convert md file
     markdown_content = Path(template.filename).read_text()
     text = markdowner.convert(markdown_content)
     title = markdowner.Meta["title"][0]
 
-    site_dir_names = []
-    path = template.name.split("/")
-    target = dirs
+    links_text = [] # List to store the text of links in the navbar (in order)
+    split_path = template.name.split("/") # Split path to current file
+    target = dirs # Set searching directory from root
+    
 
     # Iteratively walk the known directories
     # Since staticjinja walks the directory in order, and due to how the website is structured, here we can assume every access to the dict/lists is valid
-    for i in path[:-2]: # Divide path to get folders
-        for j in target["children"]: # Iterate current directory to find the subdirectory
+    for i in split_path[:-2]: # Iterate folders up until the previous folder (last item is always "!index.html")
+        for j in target["children"]: # Iterate searching directory to find the corresponding subdirectory
             if j["dir"] == i:
-                target = j
-                site_dir_names.append(target["name"]) # Append the "fancy" name of the directory
+                target = j # Change searching directory to the one just found
+                links_text.append(target["name"]) # Append the link text to the list
                 break
 
     # Due to how the website is structured, we can always assume the last directory has to be added
-    if len(path) > 1:
-        target["children"].append(dit_dict(title, path[-2]))
+    if len(split_path) > 1:
+        target["children"].append(dir_dict(title, split_path[-2]))
         target = target["children"][-1]
-        site_dir_names.append(target["name"])
+        links_text.append(target["name"])
 
     # Create the nav header
     SEPARATOR = """<span class="header-nav-separator">&gt;</span>"""
     href = "/"
     nav = f"""<a href="{href}"><span class="material-symbols-outlined">home</span> Home</a>""" # Home
     # Create links for all but the last directory
-    for i in range(len(path) - 2):
-        href += f"{path[i]}/"
-        nav += f"""{SEPARATOR}<a href="{href}">{site_dir_names[i]}</a>"""
+    for i in range(len(split_path) - 2):
+        href += f"{split_path[i]}/"
+        nav += f"""{SEPARATOR}<a href="{href}">{links_text[i]}</a>"""
 
     # Create non-link text for the last (current) directory
-    if len(path) > 1:
-        nav += f"""{SEPARATOR}<span>{site_dir_names[-1]}</span>"""
+    if len(split_path) > 1:
+        nav += f"""{SEPARATOR}<span>{links_text[-1]}</span>"""
 
     return {
         "text": text,
@@ -96,11 +98,20 @@ site = Site.make_site(
     rules=[(r".*\.md", render_md)],
 )
 
-# Initialize the fancy home link
-dirs = dit_dict("""<span class="material-symbols-outlined">home</span>Home""", "")
+def main():
+    # Initialize the fancy home link
+    global dirs
+    dirs = dir_dict("""<span class="material-symbols-outlined">home</span>Home""", "")
 
-staticjinja.logger.setLevel(logging.DEBUG)
-shutil.rmtree(OUTPATH, ignore_errors=True)
-os.makedirs(OUTPATH, exist_ok=True)
-shutil.copytree(STATICPATH, OUTPATH, dirs_exist_ok=True)
-site.render()
+    staticjinja.logger.setLevel(logging.DEBUG)
+    # Delete output directory
+    shutil.rmtree(OUTPATH, ignore_errors=True)
+    # Create output directory
+    os.makedirs(OUTPATH, exist_ok=True)
+    # Copy content from static directory
+    shutil.copytree(STATICPATH, OUTPATH, dirs_exist_ok=True)
+    # Render templates
+    site.render()
+
+if __name__ == "__main__":
+    main()
